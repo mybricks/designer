497a7b7bc1d626e6d0d054b825fca717da452005
CMJ <cmj@CMJs-MacBook-Pro.local>
2021-07-08 14:56:08 +0800

    refactor css

--- desn-comlib-view/src/index.tsx
+++ desn-comlib-view/src/index.tsx
@@ -14,7 +14,7 @@ import OutlineView from './outline/OutlineView'
 import {useObservable} from "@mybricks/rxui";
 
 class MyContext {
-  activeTabId: string = 'outline'
+  activeTabId: string = 'coms'
 
   isActiveCatelog(id) {
     return this.activeTabId === id
